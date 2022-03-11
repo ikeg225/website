0b177287e0eb100fa0a885cbded27e2bb5bf1f35
Ethan Ikegami <ikeg225@berkeley.edu>
2022-03-10 20:43:21 -0800

    sticky header

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import '../css/footer.css';
 
-function Footer() {
+function Footer() {    
     return (
         <footer>
             <p>Built from scratch using ReactJS. Checkout the <a href="https://github.com/ikeg225/work-website" className="hover-anim" target="_blank" rel="noopener noreferrer">GitHub Repo</a>.</p>
--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,7 @@ import { useState } from "react";
 function Header() {
     const activeLink = ({ isActive }) => isActive ? 'activeLink' : 'hover-anim'
     const [active, setActive] = useState(false);
+    const [navBar, setNavBar] = useState(false);
 
     function activate() {
         if (window.screen.width <= 425) {
@@ -28,18 +29,28 @@ function Header() {
         }
     }
 
+    function navBarBackground() {
+        if (window.scrollY >= 600) {
+            setNavBar(true)
+        } else {
+            setNavBar(false)
+        }
+    }
+
+    window.addEventListener("scroll", navBarBackground)
+
     return (
-        <header>
-            <nav>
+        <header className={navBar ? "active" : ""}>
+            <nav className={navBar ? "active" : ""}>
                 <ul className={addActive("leftNav")} onClick={activate}>
                     <li><NavLink to="/" className={activeLink}>Home</NavLink></li>
                     <li><NavLink to="/projects" className={activeLink}>Projects</NavLink></li>
                     <li><NavLink to="/contact" className={activeLink}>Contact</NavLink></li>
                 </ul>
-                <div class={addActive("hamburger")} onClick={activate}>
-                    <span class={addActive("bar left")}></span>
-                    <span class={addActive("bar gone")}></span>
-                    <span class={addActive("bar right")}></span>
+                <div className={addActive("hamburger")} onClick={activate}>
+                    <span className={addActive("bar left")}></span>
+                    <span className={addActive("bar gone")}></span>
+                    <span className={addActive("bar right")}></span>
                 </div>
                 <ul className="rightNav">
                     <li className="hover-anim"><a href="https://www.linkedin.com/in/ethan-ikegami/" target="_blank" rel="noopener noreferrer"><img src={linkedinLogo} width="30px" className="linkedin" /></a></li>
