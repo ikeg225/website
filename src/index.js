bfa664759130f762e08cd473a6f63d5361bd0089
Ethan Ikegami <ikeg225@berkeley.edu>
2022-05-14 19:27:04 -0700

    analytics

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 //import ReactDOM from 'react-dom';
 import App from './App';
 import { hydrate, render } from "react-dom";
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-WY5HW5RE3T");
+ReactGA.send("pageview");
 
 const rootElement = document.getElementById("root");
 if (rootElement.hasChildNodes()) {
